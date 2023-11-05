127c8cd7564e0adc19a402e2315382caab097ce5
Bakalov Vitaliy <bakalov.vitaliy@gmail.com>
2023-11-05 22:05:14 +0700

    Reworked consultation section

--- components/features/ConsultationSection.vue
+++ components/features/ConsultationSection.vue
@@ -9,103 +9,33 @@
       </template>
     </i18n-t>
 
-    <ul class="questions">
-      <li
-        v-for="(question, index) in $tm('consultation.questions')"
+    <div class="questions">
+      <UiConsultationCard
+        v-for="({ title, icon}, index) in $tm('consultation.questions')"
         :key="index"
-        class="consultation-section__question"
-      >
-        <span>{{ question }}</span>
-      </li>
-    </ul>
+        :title="title"
+        :icon="icon"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { $gsap } = useNuxtApp()
-let timeline: gsap.core.Timeline | null = null
-
-onMounted(() => {
-  if (window.matchMedia('(min-width: 768px)').matches) {
-    loadDesktopAnimation()
-  } else {
-    loadMobileAnimation()
-  }
-})
-
-const loadMobileAnimation = () => {
-  timeline = $gsap.timeline({
-    scrollTrigger: {
-      trigger: '.consultation-section',
-      start: 'top 30%'
-    }
-  })
-
-  timeline
-    .from('.consultation-section__question', {
-      opacity: 0,
-      x: '-20px',
-      stagger: 0.3
-    })
-}
-
-const loadDesktopAnimation = () => {
-  timeline = $gsap.timeline({
-    scrollTrigger: {
-      trigger: '.consultation-section',
-      start: 'top 30%'
-    }
-  })
-
-  timeline
-    .from('.consultation-section__question', {
-      opacity: 0,
-      x: '-20px',
-      stagger: 0.2
-    })
-}
 </script>
 
 <style lang="scss" scoped>
-$questions-gap: 30px;
-
 .consultation-section {
   padding: 10vh $content-padding;
-  box-sizing: border-box;
 
   .questions {
-    @media screen and (min-width: $md) {
-      padding: 0;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      flex-wrap: wrap;
-      row-gap: 80px;
-    }
-  }
+    display: flex;
+    row-gap: 20px;
+    flex-wrap: wrap;
+    justify-content: center;
 
-  &__question {
     @media screen and (min-width: $md) {
-      display: flex;
-      justify-content: center;
-      text-align: center;
-      list-style-type: none;
-      width: 100%;
-      max-width: calc(100% / 3 - $questions-gap);
-      position: relative;
-
-      &::before {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 50px;
-        background: $primary-background;
-        z-index: -1;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%) rotate(-5deg);
-        border-radius: 30px;
-      }
+      column-gap: 2%;
+      justify-content: space-around;
     }
   }
 }
---
+++ components/ui/ConsultationCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="consultation-card">
+    <i :class="`icon ic-${icon}`" />
+    <h3 class="title">
+      {{ title }}
+    </h3>
+  </div>
+</template>
+
+<script lang="ts" setup>
+export interface Props {
+  icon: string,
+  title: string
+}
+
+defineProps<Props>()
+</script>
+
+<style lang="scss" scoped>
+.consultation-card {
+  border: 2px solid $primary-color;
+  border-radius: $base-border-radius;
+  padding: 30px;
+  width: 100%;
+  gap: 30px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  max-width: 450px;
+
+  @media screen and (min-width: $md) {
+    width: 49%;
+  }
+
+  @media screen and (min-width: $lg) {
+    width: 32%;
+  }
+
+  @media screen and (min-width: $xl) {
+    border: 0.13vw solid $primary-color;
+    border-radius: 0.5vw;
+    padding: 1.5vw;
+    width: 20vw;
+    max-width: unset;
+  }
+
+  .icon {
+    font-size: 50px;
+    color: $primary-color;
+
+    @media screen and (min-width: $xl) {
+      font-size: 2.6vw;
+    }
+  }
+
+  .title {
+    text-align: center;
+    margin: 0;
+
+    @media screen and (min-width: $xl) {
+      font-size: 1.1vw;
+    }
+  }
+}
+</style>
