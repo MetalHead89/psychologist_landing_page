07da14dd78a8996a8f2636a911e23c9af82124bb
Vitalij Bakalov <vbakalov@inno.co>
2023-11-11 20:34:43 +0700

    Metric id moved to variable

--- composables/metrika.ts
+++ composables/metrika.ts
@@ -1,8 +1,8 @@
 export function useMetrika() {
-  // const config = useRuntimeConfig()
+  const config = useRuntimeConfig()
 
   const metrika = (method: string, ...options: any) => {
-    ym('95436575', method, ...options)
+    ym(config.public.metrikaId, method, ...options)
   }
 
   return { metrika }
