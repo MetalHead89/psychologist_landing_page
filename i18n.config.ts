c350e875223b1c81bb11e6ba034a83a369c1c9e4
Bakalov Vitaliy <bakalov.vitaliy@gmail.com>
2024-09-01 20:57:03 +0700

    Updated node npm and nuxt

--- i18n.config.ts
+++ i18n.config.ts
@@ -2,7 +2,9 @@ import ru from './locales/ru/common'
 
 export default defineI18nConfig(() => ({
   legacy: false,
+  availableLocales: ['ru'],
   locale: 'ru',
+  fallbackLocale: 'ru',
   messages: {
     ru
   }
